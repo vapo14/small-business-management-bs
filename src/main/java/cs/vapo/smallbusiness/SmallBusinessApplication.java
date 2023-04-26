d89ae5d46666ccd986f842c4ac38878b67c558f2
Víctor Padrón <49078518+vapo14@users.noreply.github.com>
2023-04-25 22:26:14 -0600

    added spring boot initial config

---
+++ src/main/java/cs/vapo/smallbusiness/SmallBusinessApplication.java
@@ -0,0 +1,13 @@
+package cs.vapo.smallbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/vapo/smallbusiness/SmallBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vapo.smallbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
